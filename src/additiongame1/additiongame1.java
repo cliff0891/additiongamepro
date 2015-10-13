35d122d50cd4c68350fad28ce5ab4066f89f7e34
cliff0891 <yuyunc@student.swosu.edu>
2015-10-13 17:21:21 -0500

    added lines of comments

--- src/additiongame1/additiongame1.java
+++ src/additiongame1/additiongame1.java
@@ -1,4 +1,5 @@
 package additiongame1;
+import java.util.Scanner;
 
 public class additiongame1 {
 
@@ -6,6 +7,69 @@ public class additiongame1 {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello Class!");
+		
+		// Round 1
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
+		     
+		// Round 2
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
+		
+		// Round 3
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
+		
+		// Round 4
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
+		
+				
+		
 	}
 
 }
